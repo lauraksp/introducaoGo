0344791b9c8e63dab838d39dbdcbd60cb2d7eded
Laura Kimberly <lauraksp@icloud.com>
2023-05-05 23:44:50 -0300

    implementando idade

--- slice/slice.go
+++ slice/slice.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Ola")
 	var nomes = []string{"Laura", "Kimberly", "Joao"}
+	var idades = []int{18, 20, 22}
 
 	for i, v := range nomes {
-		fmt.Printf("ola %s voce está na posicao numero: %d\n", v, i)
+		fmt.Printf("ola %s voce está na posicao numero: %d, e sua idade é %d\n", v, i, idades[i])
 	}
 }
