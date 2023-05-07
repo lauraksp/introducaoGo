4f3d8dc7c4d5240bd9c88d75d852f5a9383bb732
Laura Kimberly <lauraksp@icloud.com>
2023-05-07 18:02:23 -0300

    cadastraCPF

--- iinterface/iinterface.go
+++ iinterface/iinterface.go
@@ -17,6 +17,11 @@ func (p *Pessoa) Fala(texto string) {
 	fmt.Println(texto)
 }
 
+func cadastrarCPF(h HUmano) {
+	h.CPF = "15100521608"
+	fmt.Println("CPF cadastrado")
+}
+
 func main() {
 
 	p := Pessoa{}
